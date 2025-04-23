6e2e2b137634064e2789f51e7c70857ed2e31818
Pablo Valentin <95161515+PabloValentin94@users.noreply.github.com>
2025-04-23 17:05:22 -0300

    Update 06.js

--- NoSQL/Provas/2025-04-23/Atividades/06.js
+++ NoSQL/Provas/2025-04-23/Atividades/06.js
@@ -3,7 +3,22 @@ db.usuarios.aggregate([
     {$addFields: {
 
         total_gasto: {$sum: "$compras"},
-        media_compra: {$avg: "$compras"},
+        media_compra: {$avg: "$compras"}
+
+    }},
+
+    /*
+
+        Não é possível referenciar um campo que ainda está em construção pelo 
+        estágio da pipeline. Devido a isso, é preciso dividir a lógica em dois 
+        estágios (O estágio anterior define o campo que será usado, enquanto o 
+        estágio a seguir, irá definir um novo campo com base no valor do campo 
+        gerado, que agora já existe.).
+
+    */
+
+    {$addFields: {
+
         classificacao_gasto: {$cond: {
             
             if: {$gte: ["$total_gasto", 200]},
